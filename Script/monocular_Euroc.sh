af3ca57943de6bedf1c7311b4a2f72795aad1ad6
TouchDeeper <610690824@qq.com>
2020-02-04 19:37:30 +0800

    backup

---
+++ Script/monocular_Euroc.sh
@@ -0,0 +1,2 @@
+cd ..
+./Examples/Monocular/mono_euroc Vocabulary/ORBvoc.txt Examples/Monocular/EuRoC.yaml /home/td/orb/MH_03_medium/mav0/cam0/data Examples/Monocular/EuRoC_TimeStamps/MH03.txt
---
+++ Script/stereo_Euroc.sh
@@ -0,0 +1,2 @@
+cd ..
+./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml /home/td/orb/MH_03_medium/mav0/cam0/data /home/td/orb/MH_03_medium/mav0/cam1/data Examples/Stereo/EuRoC_TimeStamps/MH03.txt
